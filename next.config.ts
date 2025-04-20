7696dec75fb756c863075d0e1c72d4e84787bd12
yoohaso <solhanyun@gmail.com>
2025-04-20 22:45:01 +0900

    chore: styled-components 설치 및 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;

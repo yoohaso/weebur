ba038a0a898fbc440960a5a1b5e78305425f5b3b
yoohaso <solhanyun@gmail.com>
2025-04-25 00:56:11 +0900

    refactor: search page에서 데이터 로직을 container로 분리

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,13 +1,58 @@
 import { useRandomViewMode } from '@/hooks/useRandomViewMode';
-import { dehydrate, QueryClient, useInfiniteQuery } from '@tanstack/react-query';
-import { useRouter } from 'next/router';
+import { dehydrate, QueryClient, useInfiniteQuery, UseInfiniteQueryResult } from '@tanstack/react-query';
 import { GetServerSidePropsContext } from 'next';
 import { ProductsView } from '@/features/ProductsView';
 import { InfiniteScroll } from '@/components/InfiniteScroll';
-import { fetchProductsBySearch } from '@/api/product';
+import { fetchProductsBySearch, Product } from '@/api/product';
 import { PAGINATION_LIMIT } from '@/constants';
 import { productKeys } from '@/api/queryKeyFactory';
 import { SearchForm } from '@/features/SearchForm';
+import { useRouter } from 'next/router';
+
+interface ContainerProps {
+  children: (props: {
+    products: Product[];
+    hasNextPage: UseInfiniteQueryResult['hasNextPage'];
+    isFetchingNextPage: UseInfiniteQueryResult['isFetchingNextPage'];
+    fetchNextPage: UseInfiniteQueryResult<Product[]>['fetchNextPage'];
+  }) => React.ReactNode;
+}
+
+export function Container({ children }: ContainerProps) {
+  const router = useRouter();
+  const { q: search } = router.query as { q: string };
+
+  const {
+    data: products,
+    isPending,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery({
+    initialPageParam: 0,
+    queryKey: productKeys.list(search),
+    queryFn: ({ pageParam }) => fetchProductsBySearch({ search, skip: pageParam }),
+    getNextPageParam: lastPage => {
+      if (lastPage.limit < PAGINATION_LIMIT) {
+        return;
+      }
+
+      return lastPage.skip + PAGINATION_LIMIT;
+    },
+    select: data => data.pages.flatMap(page => page.products),
+  });
+
+  if (isPending || !products) {
+    return <div>로딩 중...</div>;
+  }
+
+  return children({
+    products,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  });
+}
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
   const queryClient = new QueryClient();
@@ -32,54 +77,30 @@ export default function SearchPage() {
   const router = useRouter();
   const { q: search } = router.query as { q: string };
 
-  const {
-    data: products,
-    isPending,
-    hasNextPage,
-    isFetchingNextPage,
-    fetchNextPage,
-  } = useInfiniteQuery({
-    initialPageParam: 0,
-    queryKey: productKeys.list(search),
-    queryFn: ({ pageParam }) => fetchProductsBySearch({ search, skip: pageParam }),
-    getNextPageParam: lastPage => {
-      if (lastPage.limit < PAGINATION_LIMIT) {
-        return;
-      }
-
-      return lastPage.skip + PAGINATION_LIMIT;
-    },
-    select: data => data.pages.flatMap(page => page.products),
-  });
-
-  const handleIntersect = () => {
-    if (hasNextPage && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  };
-
   if (!viewMode) {
     return null;
-  }
-
-  if (isPending || !products) {
-    return <div>Loading...</div>;
   }
 
   return (
     <>
       <SearchForm initialValue={search} />
-      {products.length === 0 ? (
-        <p>일치하는 결과가 없습니다.</p>
-      ) : (
-        <InfiniteScroll
-          onIntersect={handleIntersect}
-          disabled={!hasNextPage && products.length > PAGINATION_LIMIT}
-          disabledComponent={<p>더 이상 불러올 수 없습니다.</p>}
-        >
-          <ProductsView viewMode={viewMode} products={products} />
-        </InfiniteScroll>
-      )}
+      <Container>
+        {({ products, hasNextPage, isFetchingNextPage, fetchNextPage }) => (
+          <>
+            {products.length === 0 ? (
+              <p>일치하는 결과가 없습니다.</p>
+            ) : (
+              <InfiniteScroll
+                onIntersect={() => hasNextPage && !isFetchingNextPage && fetchNextPage()}
+                disabled={!hasNextPage && products.length > PAGINATION_LIMIT}
+                disabledComponent={<p>더 이상 불러올 수 없습니다.</p>}
+              >
+                <ProductsView viewMode={viewMode} products={products} />
+              </InfiniteScroll>
+            )}
+          </>
+        )}
+      </Container>
     </>
   );
 }

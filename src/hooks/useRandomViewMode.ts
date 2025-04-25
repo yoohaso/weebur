90e0dc44ff3e9795329fe445564d080d3a9f6db9
yoohaso <solhanyun@gmail.com>
2025-04-25 16:10:38 +0900

    feat: 화면 포커즈 시 뷰 모드 만료 시간 확인 후 처리

--- src/hooks/useRandomViewMode.ts
+++ src/hooks/useRandomViewMode.ts
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 type ViewMode = 'LIST' | 'GRID';
+const VIEW_MODE = 'viewMode';
 
 interface StoredViewMode {
   viewMode: ViewMode;
@@ -19,11 +20,11 @@ const storeViewModeInLocalStorage = (viewMode: ViewMode): void => {
   const ONE_DAY_IN_MS = 1 * 24 * 60 * 60 * 1000;
   const expiryTime = new Date().getTime() + ONE_DAY_IN_MS;
 
-  localStorage.setItem('viewMode', JSON.stringify({ viewMode, expiryTime }));
+  localStorage.setItem(VIEW_MODE, JSON.stringify({ viewMode, expiryTime }));
 };
 
 const getViewModeFromLocalStorage = (): StoredViewMode | undefined => {
-  const storedViewMode = localStorage.getItem('viewMode');
+  const storedViewMode = localStorage.getItem(VIEW_MODE);
 
   if (!storedViewMode) {
     return undefined;
@@ -32,7 +33,7 @@ const getViewModeFromLocalStorage = (): StoredViewMode | undefined => {
   const { viewMode, expiryTime } = JSON.parse(storedViewMode);
 
   if (new Date().getTime() > expiryTime) {
-    localStorage.removeItem('viewMode');
+    localStorage.removeItem(VIEW_MODE);
     return undefined;
   }
 
@@ -42,18 +43,44 @@ const getViewModeFromLocalStorage = (): StoredViewMode | undefined => {
 export function useRandomViewMode(): [ViewMode | undefined] {
   const [viewMode, setViewMode] = useState<ViewMode | undefined>();
 
-  useEffect(() => {
-    const storedViewMode = getViewModeFromLocalStorage();
-
-    if (!storedViewMode) {
-      const newViewMode = getRandomViewMode();
-      storeViewModeInLocalStorage(newViewMode);
-      setViewMode(newViewMode);
-    } else {
-      const { viewMode } = storedViewMode;
-      setViewMode(viewMode);
-    }
+  const setPersistViewMode = useCallback(() => {
+    const newViewMode = getRandomViewMode();
+    storeViewModeInLocalStorage(newViewMode);
+    setViewMode(newViewMode);
   }, []);
+
+  useEffect(
+    function initializeViewMode() {
+      const storedViewMode = getViewModeFromLocalStorage();
+
+      if (!storedViewMode) {
+        setPersistViewMode();
+      } else {
+        const { viewMode } = storedViewMode;
+        setViewMode(viewMode);
+      }
+    },
+    [setPersistViewMode]
+  );
+
+  useEffect(
+    function initializeViewModeOnFocus() {
+      const handleFocus = () => {
+        const viewMode = getViewModeFromLocalStorage();
+
+        if (viewMode === undefined) {
+          setPersistViewMode();
+        }
+      };
+
+      window.addEventListener('focus', handleFocus);
+
+      return () => {
+        window.removeEventListener('focus', handleFocus);
+      };
+    },
+    [setPersistViewMode]
+  );
 
   return [viewMode];
 }

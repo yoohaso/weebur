66913eed88df26479e76c6c85919f4a82b63df1a
yoohaso <solhanyun@gmail.com>
2025-04-25 13:36:49 +0900

    design: SearchForm 스타일 개선

--- src/features/SearchForm.tsx
+++ src/features/SearchForm.tsx
@@ -1,5 +1,6 @@
 import { Field, Form, Formik } from 'formik';
 import { useRouter } from 'next/router';
+import styled from 'styled-components';
 
 interface SearchFormProps {
   initialValue?: string;
@@ -17,13 +18,27 @@ export function SearchForm({ initialValue = '' }: SearchFormProps) {
       }}
     >
       {({ isSubmitting }) => (
-        <Form>
-          <Field type="search" name="search" />
-          <button type="submit" disabled={isSubmitting}>
+        <StyledForm>
+          <StyledField type="search" name="search" />
+          <StyledButton type="submit" disabled={isSubmitting}>
             검색
-          </button>
-        </Form>
+          </StyledButton>
+        </StyledForm>
       )}
     </Formik>
   );
 }
+
+const StyledField = styled(Field)`
+  width: 100%;
+  margin-right: 10px;
+`;
+
+const StyledForm = styled(Form)`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const StyledButton = styled.button`
+  width: 80px;
+`;
